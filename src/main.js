4b5133eabb34cdf8a6873ac637bb4ebb08c2e86b
James Bonaguro <james.bonaguro@gmail.com>
2026-04-30 01:53:40 -0500

    Use standard React DOM client entry

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from '@esm-bundle/react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App.js';
 import './styles.css';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   React.createElement(
     React.StrictMode,
     null,
     React.createElement(App),
   ),
-  document.getElementById('root'),
 );
